e5004d0f2b1aad92150497ea6c00996694dda101
Pidburachynskyi Rostyslav <r.pidburachynskyi@apiko.com>
2021-01-22 20:29:26 +0200

    Add Wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import "./App.css";
+import MultipleUploader from "./components/MultipleUploader";
 import SingleUploader from "./components/SingleUploader";
 
 function App() {
   return (
     <div className="App">
-      <SingleUploader />
+      <div className="Wrapper">
+        <h1>Multiple uploader</h1>
+        <MultipleUploader />
+      </div>
+      <div className="Wrapper">
+        <h1>Single uploader</h1>
+        <SingleUploader />
+      </div>
     </div>
   );
 }
